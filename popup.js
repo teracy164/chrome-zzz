5f5efb12b4fa132d032869675c6c4a8c2a4ce148
teracy164 <literacy1945@gmail.com>
2024-07-30 22:11:53 +0900

    不要なコードを削除

--- popup.js
+++ popup.js
@@ -1,18 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const switchElement = document.getElementById('switch');
-
-    // ストレージからスイッチの状態を読み込む
-    chrome.storage.sync.get('switchState', (data) => {
-        switchElement.checked = data.switchState || false;
-    });
-
-    // スイッチの状態が変わったときにストレージに保存する
-    switchElement.addEventListener('change', () => {
-        chrome.storage.sync.set({ switchState: switchElement.checked }, () => {
-            console.log('Switch state saved:', switchElement.checked);
-        });
-    });
-
     document.getElementById('start').addEventListener('click', () => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             console.log('tabs', tabs)
